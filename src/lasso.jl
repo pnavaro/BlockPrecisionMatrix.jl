9e90f716946d40e5e5ff3ef2bff1a3e042e3ac41
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-09-27 18:50:56 +0200

    Update lasso.jl

--- src/lasso.jl
+++ src/lasso.jl
@@ -1,3 +1,5 @@
+using Statistics
+
 """ Regularization for Lasso Regression """
 struct L1Regularization
     alpha :: Float64
@@ -35,59 +37,48 @@ struct LassoRegression
     regularization :: L1Regularization
     n_iterations   :: Int64
     learning_rate  :: Float64
-    w              :: Vector{Float64}
 
-    function LassoRegression( ; degree, reg_factor, n_features )
+    function LassoRegression( ; degree, reg_factor)
 
         n_iterations  = 3000
         learning_rate = 0.01
-        w             = zeros(Float64, n_features) 
  
         new( degree, L1Regularization(reg_factor), n_iterations, 
-             learning_rate, w)
+             learning_rate)
 
     end
 
-    function LassoRegression( ; degree, reg_factor, n_features, 
+    function LassoRegression( ; degree, reg_factor, 
                               learning_rate, n_iterations)
 
-        w  = zeros(Float64, n_features) 
         new( degree, L1Regularization(reg_factor), n_iterations, 
-             learning_rate, w)
+             learning_rate)
 
     end
 
 end
 
-function fit(self, X, y)
+function fit_and_predict(self, X, y)
+
+    @show size(y)
     X = normalize(polynomial_features(X, self.degree))
     # Insert constant ones for bias weights
     X = hcat( ones(eltype(X), size(X)[1]), X)
-    self.training_errors = Float64[]
     n_features = size(X)[2]
 
     # Initialize weights randomly [-1/N, 1/N]
     limit = 1 / sqrt(n_features)
-    self.w .= -limit + 2 .* limit .* rand(n_features)
+    w = -limit .+ 2 .* limit .* rand(n_features)
 
     # Do gradient descent for n_iterations
     for i in 1:self.n_iterations
-        y_pred = X * self.w
-        # Calculate l2 loss
-        mse = mean(0.5 .* (y .- y_pred).^2 + regularization(self.w))
-        psuh!(self.training_errors, mse)
+        y_pred = X * w
         # Gradient of l2 loss w.r.t w
-        grad_w  = - (y .- y_pred) * X .+ grad(regularization, self.w)
+        grad_w  = - ((y .- y_pred)' * X)' .+ grad(self.regularization, w)
         # Update the weights
-        self.w .-= self.learning_rate .* grad_w
+        w .-= self.learning_rate .* grad_w
     end
 
-end
+    return X * w
 
-function predict(self, X)
-    X = normalize(polynomial_features(X, self.degree))
-    # Insert constant ones for bias weights
-    X = hcat( ones(eltype(X), size(X)[1]), X)
-    y_pred = X * self.w
-    return y_pred
 end
