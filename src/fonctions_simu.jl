1402abdfb5c54e493df22c32f6d6535037ac0244
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-09-27 16:23:52 +0200

    Update fonctions_simu.jl

--- src/fonctions_simu.jl
+++ src/fonctions_simu.jl
@@ -1,8 +1,4 @@
-###########################
-# fonctions de simulation #
-###########################
-
-using  Random, StatsBase, Rotations
+using  Random, StatsBase
 import Base.Iterators: flatten
 using  LinearAlgebra
 
@@ -10,18 +6,22 @@ using  LinearAlgebra
     structure_cov(p, blocsOn; b = 10, seed = p)
 
 # Fonction pour definir les indices des blocs
+
   - p : dimension des donnees
   - b : nombre de blocs
   - blocsOn : liste des indices des couples de blocs "allumes"
 """
-function structure_cov(p, blocsOn; b = 10, seed = p)
+function structure_cov(p :: Int64, 
+                       b :: Int64, 
+                       blocsOn :: Vector{Vector{Int64}}; 
+                       seed = 0 :: Int64 )
   
   rng = MersenneTwister(seed)
   
-  blocs = sample(rng, 3:(p-2), size = (b-1), replace=false)
+  blocs = sample(rng, 3:(p-2), (b-1), replace=false)
   sort!(blocs)
-  blocs = [1, blocs..., p]
-  indblocs = Int64[]
+  @show blocs = [1, blocs..., p]
+  indblocs = UnitRange{Int64}[]
   push!(indblocs, 1:blocs[2])
   for i in 2:(length(blocs)-1) 
     push!(indblocs, (blocs[i]+1):blocs[i+1])
@@ -42,8 +42,7 @@ function structure_cov(p, blocsOn; b = 10, seed = p)
       end
   end
   
-  return Dict(:blocs => blocs, :indblocs => indblocs, 
-              :matBlocs => matBlocs, :blocsOn => blocsOn)
+  return Dict(:blocs => blocs, :indblocs => indblocs, :matBlocs => matBlocs)
 
 end
 
@@ -64,30 +63,30 @@ function cov_simu(blocs, indblocs, blocsOn, D)
   P = zeros(Float64, (p, p))
   
   # matrices de rotation pour les blocs "allumes"
-  for i in 1:length(blocsOn)
-    B1 = indblocs[blocsOn[i][1]][1]:indblocs[blocsOn[i][1]][length(indblocs[blocsOn[i][1]])]
-    B2 = indblocs[blocsOn[i][2]][1]:indblocs[blocsOn[i][2]][length(indblocs[blocsOn[i][2]])]
-    nB = length(indblocs[blocsOn[i][1]])+length(indblocs[blocsOn[i][2]])
-    P[[B1..., B2...], [B1..., B2...]] .= rand(RotMatrix{nB})
+  for i in eachindex(blocsOn)
+      B1 = indblocs[blocsOn[i][1]]
+      B2 = indblocs[blocsOn[i][2]]
+      nB = length(B1) + length(B2)
+      P[[B1..., B2...], [B1..., B2...]] .= RotationMatrix(nB)
   end
   
   # matrices de rotation pour les autre blocs centraux (non allumes)
   for i in setdiff(1:b, flatten(blocsOn))
-    P[indblocs[[i]][1]:indblocs[[i]][length(indblocs[[i]])], 
-      indblocs[[i]][1]:indblocs[[i]][length(indblocs[[i]])]] .= rand(RotMatrix{length(indblocs[i])})
+      P[indblocs[i], indblocs[i]] .= RotationMatrix(length(indblocs[i]))
   end
   
   for i in 1:length(blocsOn)
-    P[indblocs[blocsOn[i][1]][1]:indblocs[blocsOn[i][1]][length(indblocs[blocsOn[i][1]])] , indblocs[blocsOn[i][2]][1]:indblocs[blocsOn[i][2]][length(indblocs[blocsOn[i][2]])]] .= 0
+    @show indblocs[i]
+    P[indblocs[blocsOn[i][1]] , indblocs[blocsOn[i][2]]] .= 0
   end
 
-  transpose!(P)
+  P .= transpose(P)
   
   # Matrice de covariance
   CovMat = P' * Diagonal(D) * P
   
   # Matrice de precision
-  PreMat = P' * Diagonal(1/D) * P
+  PreMat = P' * Diagonal(1 ./ D) * P
   
   return Dict(:CovMat => CovMat, :PreMat => PreMat)
 
