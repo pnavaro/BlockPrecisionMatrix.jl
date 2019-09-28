1c318a454ad9d5835b36988e7140749aab564265
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-09-28 13:48:12 +0200

    Update lasso_regression.jl

--- test/lasso_regression.jl
+++ test/lasso_regression.jl
@@ -1,28 +1,16 @@
-# +
+# -*- coding: utf-8 -*-
+# + {}
 using Plots, CSV
 
-# Load temperature data
-data = CSV.read(download("https://raw.githubusercontent.com/eriklindernoren/ML-From-Scratch/master/mlfromscratch/data/TempLinkoping2016.txt"), delim="\t");
-# -
-
-time = reshape(collect(data.time), size(data)[1], 1)
-temp = collect(data.temp);
-
-X = time # fraction of the year [0, 1]
-y = temp;
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
-
-scatter( X_train, y_train)
-scatter!( X_test, y_test)
-
-size(y)
+time = 1:0.1:2
+X = reshape(collect(time), size(time)[1], 1)
+n = size(X)
+y = sin.(2π * collect(time)) #.+ 0.2 *rand(n)
+scatter(X, y)
 
 # +
-include("../src/utils.jl")
 include("../src/lasso.jl")
-
-model = LassoRegression(degree=10, 
+model = LassoRegression(degree=15, 
                         reg_factor=0.05,
                         learning_rate=0.001,
                         n_iterations=4000)
@@ -32,5 +20,20 @@ y_pred = fit_and_predict(model, X, y);
 plot(X, y_pred)
 scatter!(X, y)
 # -
+X = reshape(1:5,5,1)
+
+
+normalize(polynomial_features(X, 3))
+
+X = rand(10,4)
+
+y = collect(1:10)
+
+(y' * X)'
+
+X * y
+
+
+
 
 
