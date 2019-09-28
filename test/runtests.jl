6b7df331d67ddea58bee032526d76d45ccae5a93
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-09-28 16:18:34 +0200

    Move R files in R directory

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,18 +15,3 @@ using Test, LinearAlgebra
     end
 
 end
-
-p        = 20 # c(20, 100)
-n        = 500 # c(100, 200, 500, 1000)
-b        = 3 # c(3, 5, 5, 20)
-blocsOn  = list(c(1,3))
-
-# simulation des donnees
-resBlocs = structure_cov(p, b, blocsOn, seed = 2)
-resBlocs[:blocs]
-#D        = runif(p, 10^-4, 10^-2)
-#resmat   = CovSimu(resBlocs$blocs, resBlocs$indblocs, blocsOn, D = D)
-#
-#image.plot(resmat$CovMat)
-#image.plot(resmat$PreMat)
-
