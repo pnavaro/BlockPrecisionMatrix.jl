5c377eea687ab094e093bb42b38727ff7edd5ee6
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-09-29 21:30:35 +0200

    Update compute_betas.jl

--- compute_betas.jl
+++ compute_betas.jl
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -14,17 +15,83 @@
 # ---
 
 using Distributions, LinearAlgebra, InvertedIndices
+using GLMNet, Plots
+using BenchmarkTools
 
-sigma = 2.0 * Matrix(I, 3, 3)
+# +
+using ScikitLearn
+@sk_import datasets: load_diabetes
 
-X = transpose(rand( MvNormal(sigma), 10))
+diabetes = load_diabetes()
 
-include("src/utils.jl")
-include("src/lasso.jl")
+X = diabetes["data"]
+y = diabetes["target"];
+# -
 
+"""
+Soft threshold function used for normalized 
+data and lasso regression
+"""
+function soft_threshold(rho :: Float64, lamda :: Float64)
+    if rho < - lamda
+        return (rho + lamda)
+    elseif rho >  lamda
+        return (rho - lamda)
+    else
+        return 0
+    end
+end
+
+
+# +
+"""
+Coordinate gradient descent for lasso regression.
+"""    
+function compute_betas(X, y, λ; num_iters=100)
+    
+    m, n = size(X)
+    β = ones(Float64, n)
+    mse_next = mean(0.5 * (y .- X * β).^2 .+ λ * norm(β))
+    mse_last = mse_next
+    training_errors = Float64[]
+    i = 1
+    #Looping until max number of iterations
+    while ( mse_next <= mse_last && i < num_iters )
+        #Looping through each coordinate
+        for j in 1:n
+            
+            #Vectorized implementation
+            X_j = view( X, :, j)
+            y_pred = X * β
+            rho = X_j' * (y .- y_pred  .+ β[j] .* X_j)
+        
+            β[j] = soft_threshold(rho..., λ)
+        end
+        
+        mse_last = mse_next
+        mse_next = mean(0.5 * (y .- X * β).^2 .+ λ * norm(β))
+        push!(training_errors, mse_last)
+        i += 1
+        
+    end
+        
+    β, training_errors
+            
+end
+# -
+
+λ = 0.01
+@btime β, errors = compute_betas(X, y, λ; num_iters=71)
+
+β
+
+# +
 k = 1
 nrows, ncols = size(X)
-lambda = 2*sqrt(var(X[:,k])*log(ncols)/nrows)
-model = LassoRegression( X[:,Not(k)], X[:,k], 15, lambda, 0.01, 1000)
+
+model = glmnet( X, y, lambda = [0.1], standardize=false)
+# -
+
+model.betas
 
 
