349fba5ec46945d85358cf495089503a01c7767d
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-31 17:11:04 +0100

    Create test_rotation_matrix.jl

---
+++ test/test_rotation_matrix.jl
@@ -0,0 +1,14 @@
+@testset "random rotation matrix" begin
+
+    import PrecisionMatrix: RotationMatrix
+    
+    for p in 1:17
+    
+        P = RotationMatrix(p)
+        @test P' * P  ≈ diagm(ones(p)) 
+        @test P  * P' ≈ diagm(ones(p)) 
+        @test det(P)  ≈ 1.0
+    
+    end
+
+end
