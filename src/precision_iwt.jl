9d550010b11a317bd10e3196a4d87431aba3b239
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-31 17:10:53 +0100

    Update precision_iwt.jl

--- src/precision_iwt.jl
+++ src/precision_iwt.jl
@@ -41,26 +41,26 @@ function iwt_block_precision(data, blocks; B=1000, estimation=:SCAD)
 
   end
 
-  function permute_conditional(y,n,data.complement,estimation) # uses SCAD/OLS
-
-      permutation = shuffle(1:n)
-    
-      # SCAD/OLS estimation
-      #if estimation == :LM
-      #    n = size(data_complement)[2]
-      #    fitted = lm.fit(hcat(ones(n),data.complement),y)$fitted,
-      #elseif estimation == :SCAD
-      lambda = 2 .* sqrt.(var(y[:,1]) .* log.(ncol(data_complement))/nrow(data_complement))))
-      fitted = map(y,2,SCADmod,x=data_complement,lambda=2*sqrt(var(y[,1])*log(ncol(data.complement))/nrow(data.complement))))
-      #end
-    
-      residuals = y .- fitted
-    
-      result = fitted .+ residuals[permutation,:]
-
-      return result 
-
-  end
+#PN  function permute_conditional(y,n,data.complement,estimation) # uses SCAD/OLS
+#PN
+#PN      permutation = shuffle(1:n)
+#PN    
+#PN      # SCAD/OLS estimation
+#PN      #if estimation == :LM
+#PN      #    n = size(data_complement)[2]
+#PN      #    fitted = lm.fit(hcat(ones(n),data.complement),y)$fitted,
+#PN      #elseif estimation == :SCAD
+#PN      lambda = 2 .* sqrt.(var(y[:,1]) .* log.(ncol(data_complement))/nrow(data_complement))
+#PN      fitted = [scad_mod(v, data_complement,lambda) for v in eachcol(y)]
+#PN      #end
+#PN    
+#PN      residuals = y .- fitted
+#PN    
+#PN      result = fitted .+ residuals[permutation,:]
+#PN
+#PN      return result 
+#PN
+#PN  end
   
   
   # tests on rectangles
@@ -85,7 +85,7 @@ function iwt_block_precision(data, blocks; B=1000, estimation=:SCAD)
           data_B1 = data[:,points_x] # data of the first block
           
           data_B1_array = array(data=data.B1,dim=c(n,dim(data.B1)[2],B))
-          data_B1_list = lapply(seq(dim(data.B1.array)[3]), function(x) data.B1.array[ , , x])
+#PN          #data_B1_list = map(x -> data.B1.array[:,:,x], seq(dim(data.B1.array)[3]))
           
           for iy in 1:(ix-1) # y coordinate starting point. stops before the diagonal
 
@@ -93,50 +93,50 @@ function iwt_block_precision(data, blocks; B=1000, estimation=:SCAD)
 
                   # data of the second block
                   index_y = iy:(iy+ly) # index second block
-                  points_y = find(blocks %in% index.y)
-                  data.B2 = data[,points.y]
+#PN                  points_y = find(blocks %in% index.y)
+#PN                  data_B2 = data[:,points_y]
                   
-                  index.complement = (1:nblocks)[-c(index.x,index.y)]
-                  points.complement = which(blocks %in% index.complement)
-                  data.complement = data[,points.complement]
+#PN                  index.complement = (1:nblocks)[-c(index.x,index.y)]
+#PN                  points.complement = which(blocks %in% index.complement)
+#PN                  data.complement = data[,points.complement]
                   
                   # print(paste0('x:',index.x))
                   # print(paste0('y:',index.y))
                   # print(paste0('c:', index.complement))
                   
                   # permuted data of the first block
-                  ncol = dim(data.complement)[2]
-                  
-                  if(ncol > 0){
-                    data.B1.perm.l = lapply(data.B1.list,permute.conditional,n=n,data.complement=data.complement,estimation=estimation)
-                  }else{
-                    data.B1.perm.l = lapply(data.B1.list,permute,n=n)
-                  }
-                  
-                  data.B1.perm = simplify2array(data.B1.perm.l)
-
-                  testmatrix = zeromatrix
-                  testmatrix[points.x,points.y] = 1
-                  ntests.blocks = ntests.blocks + testmatrix
-                  image.plot(ntests.blocks,main=paste0('Blocks: ',index.x,'-',index.y))
-                  image.plot(testmatrix,main=paste0('Blocks: ',index.x,'-',index.y))
-
-                  T0.tmp = stat.test(data.B1,data.B2,data.orig=data,points.x=points.x,points.y=points.y)
-
-                  Tperm.tmp = apply(data.B1.perm,3,stat.test,block2=data.B2,data.orig=data,points.x=points.x,points.y=points.y)
-                  pval.tmp = mean(Tperm.tmp >= T0.tmp)
-
-                  corrected.pval_temp = matrix(0,nrow=p,ncol=p)
-                  corrected.pval_temp[points.x,points.y] = pval.tmp
-                  corrected.pval_temp[points.y,points.x] = pval.tmp # simmetrization
-                  
-                  pval_array[1,,] = corrected.pval # old adjusted p-value
-                  pval_array[2,,] = corrected.pval_temp # p-value resulting from the test of this block
-
-                  corrected.pval = apply(pval_array,c(2,3),max) # maximization for updating the adjusted p-value
-
-                  index = apply(pval_array,c(2,3),which.max)
-                  responsible.test[which(index==2)] = paste0(paste0(index.x,collapse=','),'-',paste0(index.y,collapse=','))
+#PN                  ncol = dim(data.complement)[2]
+#PN                  
+#PN                  if(ncol > 0){
+#PN                    data.B1.perm.l = lapply(data.B1.list,permute.conditional,n=n,data.complement=data.complement,estimation=estimation)
+#PN                  }else{
+#PN                    data.B1.perm.l = lapply(data.B1.list,permute,n=n)
+#PN                  }
+#PN                  
+#PN                  data.B1.perm = simplify2array(data.B1.perm.l)
+#PN
+#PN                  testmatrix = zeromatrix
+#PN                  testmatrix[points.x,points.y] = 1
+#PN                  ntests.blocks = ntests.blocks + testmatrix
+#PN                  image.plot(ntests.blocks,main=paste0('Blocks: ',index.x,'-',index.y))
+#PN                  image.plot(testmatrix,main=paste0('Blocks: ',index.x,'-',index.y))
+#PN
+#PN                  T0.tmp = stat.test(data.B1,data.B2,data.orig=data,points.x=points.x,points.y=points.y)
+#PN
+#PN                  Tperm.tmp = apply(data.B1.perm,3,stat.test,block2=data.B2,data.orig=data,points.x=points.x,points.y=points.y)
+#PN                  pval.tmp = mean(Tperm.tmp >= T0.tmp)
+#PN
+#PN                  corrected.pval_temp = matrix(0,nrow=p,ncol=p)
+#PN                  corrected.pval_temp[points.x,points.y] = pval.tmp
+#PN                  corrected.pval_temp[points.y,points.x] = pval.tmp # simmetrization
+#PN                  
+#PN                  pval_array[1,,] = corrected.pval # old adjusted p-value
+#PN                  pval_array[2,,] = corrected.pval_temp # p-value resulting from the test of this block
+#PN
+#PN                  corrected.pval = apply(pval_array,c(2,3),max) # maximization for updating the adjusted p-value
+#PN
+#PN                  index = apply(pval_array,c(2,3),which.max)
+#PN                  responsible.test[which(index==2)] = paste0(paste0(index.x,collapse=','),'-',paste0(index.y,collapse=','))
 
               end
           end
