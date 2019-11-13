44b7c9fbc500170fcdc9e042d57eae3f4e253ec6
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-11-13 20:01:12 +0100

    Update test_gaussian.jl

--- test/test_gaussian.jl
+++ test/test_gaussian.jl
@@ -2,10 +2,9 @@
 
 using LinearAlgebra
 using Random
+using PrecisionMatrix
 
-import PrecisionMatrix: ncvreg
-
-using MultivariateStats
+Random.seed!(42)
 
 n = 50
 p = 5
@@ -14,19 +13,8 @@ X = rand(n, p)                  # feature matrix
 a0 = rand(p)                    # ground truths
 y = X * a0 + 0.1 * randn(n)     # generate response
 
-# solve using llsq
-a = llsq(X, y; bias=false)
-
-# do prediction
-yp = X * a
-
-# measure the error
-rmse = sqrt(mean(abs2.(y .- yp)))
-println("LLSQ rmse = $rmse")
-
 # solve using linear regression
 XX = hcat(ones(n), X)
-
 beta = pinv(XX) * y
 
 # do prediction
@@ -36,13 +24,14 @@ rmse = sqrt(mean(abs2.(y .- yp)))
 println("LM rmse = $rmse")
 
 λ = [0.0]
-scad = ncvreg(X, y, λ)
+scad = NCVREG(X, y, λ)
 
-yp = XX * scad 
+@test maximum(abs.(beta .- scad.beta)) < 2e-5
+
+yp = XX * scad.beta 
 
 rmse = sqrt(mean(abs2.(y .- yp)))
 println("SCAD rmse = $rmse")
 
-@test true
 
 end
