9114feb645087c9e215170c9f59da34a954cc52a
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-11-13 18:02:21 +0100

    Update test_std.jl

--- test/test_std.jl
+++ test/test_std.jl
@@ -8,9 +8,12 @@ p = 5
 l = 5
 
 X = randn(rng,(n,p))
-XX = mystd(X)
-@test all(mean(XX.values, dims=1)  .< fill(1e-13, p))
-@test all([ c' * c for c in eachcol(XX.values)] .- fill(n, p) .< 1e-13 )
+XX, center, scale = mystd(X)
+@test all(mean(XX, dims=1)  .< fill(1e-13, p))
+@test all([ c' * c for c in eachcol(XX)] .- fill(n, p) .< 1e-13 )
+
+@test all(center .- vec(mean(X,dims=1)) .< 1e-13)
+@test all(scale  .- vec(std(X,dims=1))  .< 1e-13)
 
 end
 
