bc9bf6b4375863367122c7ff435d623b1481a89d
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-11-13 20:47:23 +0100

    Update test_gaussian.jl

--- test/test_gaussian.jl
+++ test/test_gaussian.jl
@@ -1,37 +1,36 @@
 @testset "ncvreg works for linear regression" begin
 
-using LinearAlgebra
-using Random
-using PrecisionMatrix
-
-Random.seed!(42)
-
-n = 50
-p = 5
-# prepare data
-X = rand(n, p)                  # feature matrix
-a0 = rand(p)                    # ground truths
-y = X * a0 + 0.1 * randn(n)     # generate response
-
-# solve using linear regression
-XX = hcat(ones(n), X)
-beta = pinv(XX) * y
-
-# do prediction
-yp = XX * beta 
-
-rmse = sqrt(mean(abs2.(y .- yp)))
-println("LM rmse = $rmse")
-
-λ = [0.0]
-scad = NCVREG(X, y, λ)
-
-@test maximum(abs.(beta .- scad.beta)) < 2e-5
-
-yp = XX * scad.beta 
-
-rmse = sqrt(mean(abs2.(y .- yp)))
-println("SCAD rmse = $rmse")
+    using LinearAlgebra
+    using Random
+    using PrecisionMatrix
+    
+    Random.seed!(42)
+    
+    n, p = 50, 5
+    # prepare data
+    X = rand(n, p)              # feature matrix
+    a0 = rand(p)                # ground truths
+    y = X * a0 + 0.1 * randn(n) # generate response
+    
+    # solve using linear regression
+    XX = hcat(ones(n), X)
+    beta = pinv(XX) * y
+    
+    # do prediction
+    yp = XX * beta 
+    
+    rmse = sqrt(mean(abs2.(y .- yp)))
+    println("LM rmse = $rmse")
+    
+    λ = [0.0]
+    scad = NCVREG(X, y, λ)
+    
+    @test maximum(abs.(beta .- scad.beta)) < 2e-5
+    
+    yp = XX * scad.beta 
+    
+    rmse = sqrt(mean(abs2.(y .- yp)))
+    println("SCAD rmse = $rmse")
 
 
 end
