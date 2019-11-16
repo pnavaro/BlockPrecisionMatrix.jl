7677409ef866401e0ff0f4ce66179ecee28a0e19
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-11-16 23:49:42 +0100

    Update iwt_block_precision.jl

--- notebooks/iwt_block_precision.jl
+++ notebooks/iwt_block_precision.jl
@@ -73,11 +73,11 @@ function returning the fitted values pf regression with
 the SCAD penalty (used for conditional permutations)
 """
 function scad_mod(yvector, x, lambda)
-  beta = PrecisionMatrix.ncvreg(x, yvector, penalty=:SCAD, lambda=lambda)
+  scad = PrecisionMatrix.ncvreg(x, yvector, penalty=:SCAD, lambda=lambda)
     
   n = size(x)[2]
   
-  fitted = hcat(ones(n),x) .* beta
+  fitted = hcat(ones(n),x) * scad.beta
     
   return fitted
 
@@ -93,7 +93,10 @@ uses SCAD/OLS
 function permute_conditional(y, n, data_complement, estimation)
     # SCAD/OLS estimation
     if extimation == :LM
-        #fitted = lm.fit(cbind(1,data_complement),y)$fitted
+        XX = hcat(ones(n), data_complement)
+        beta = pinv(XX) * y
+        # do prediction
+        fitted = XX * beta
     elseif estimation == :SCAD
         nrows, ncols = size(data_complement)
         λ = 2*sqrt(var(y[:,1]) * log(ncols)/nrows)
@@ -105,4 +108,90 @@ function permute_conditional(y, n, data_complement, estimation)
     return fitted .+ residuals[permutation,:]
 end
 
+# +
+# tests on rectangles
+pval_array = zeros(Float64,(2,p,p))
+corrected_pval = zeros(Float64, (p,p))
+seeds = round.(100000 * rand(B))
 
+responsible_test = zeros(Float64, (p,p))
+ntests_blocks = zeros(Float64,(p,p))
+zeromatrix = zeros(Float64,(p,p))
+
+# -
+
+for ix in 2:nblocks  # x coordinate starting point.
+
+    for lx in 0:(nblocks-ix) # length on x axis of the rectangle
+        
+        index_x = ix:(ix+lx) # index first block
+        points_x = which(blocks %in% index.x) # coefficients in block index.x
+        data_B1 = data[,points.x] # data of the first block
+
+        data_B1_array = array(data=data.B1,dim=c(n,dim(data.B1)[2],B))
+        data_B1_list = lapply(seq(dim(data.B1.array)[3]), function(x) data.B1.array[ , , x])
+
+        for iy in 1:(ix-1) # y coordinate starting point. stops before the diagonal
+            for ly in 0:(ix-iy-1) # length on y axis of the rectangle
+
+                # data of the second block
+                index_y = iy:(iy+ly) # index second block
+                points_y = which(blocks %in% index.y)
+                data_B2 = data[,points.y]
+
+                index_complement = (1:nblocks)[-c(index.x,index.y)]
+                points_complement = which(blocks %in% index.complement)
+                data_complement = data[,points.complement]
+
+                # print(paste0('x:',index.x))
+                # print(paste0('y:',index.y))
+                # print(paste0('c:', index.complement))
+
+                # permuted data of the first block
+                ncol = dim(data.complement)[2]
+
+                if ncol > 0
+                  data_B1_perm_l = lapply(data_B1_list,
+                                          permute_conditional,
+                                          n=n,
+                                          data_complement=data.complement,
+                                          estimation=estimation)
+                else
+                  data_B1_perm_l = lapply(data_B1_list,permute,n=n)
+                end
+
+                data_B1_perm = simplify2array(data.B1.perm.l)
+
+                testmatrix = zeromatrix
+                testmatrix[points.x,points.y] = 1
+                ntests_blocks = ntests_blocks + testmatrix
+                plot(ntests.blocks,main=paste0('Blocks: ',index.x,'-',index.y))
+                plot(testmatrix,main=paste0('Blocks: ',index.x,'-',index.y))
+
+                T0_tmp = stat_test(data.B1,data.B2,data.orig=data,points.x=points.x,points.y=points.y)
+
+                Tperm_tmp = apply(data.B1.perm,3,stat.test,block2=data.B2,
+                                  data.orig=data,points.x=points.x,points.y=points.y)
+                pval_tmp = mean(Tperm.tmp >= T0.tmp)
+
+                corrected_pval_temp = zeros(Float64,(p,p))
+                corrected_pval_temp[points_x,points_y] = pval_tmp
+                corrected_pval_temp[points_y,points_x] = pval_tmp # simmetrization
+
+                # old adjusted p-value
+                pval_array[1,:,:] .= corrected_pval 
+                # p-value resulting from the test of this block
+                pval_array[2,:,:] .= corrected_pval_temp 
+
+                # maximization for updating the adjusted p-value
+                corrected_pval = maximum(pval_array,dims=1) 
+
+                index = zeros(Int,(p,p))
+                for i in 1:p, j in 1:p 
+                    index[i,j] = findmax(A[:,i,j])[2] 
+                end
+                responsible.test[which(index==2)] = paste0(paste0(index.x,collapse=','),'-',paste0(index.y,collapse=','))
+            end
+        end
+    end
+end
