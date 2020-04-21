b5108329f309d16775b0fcb4b525e89ae5e77844
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-04-21 16:06:09 +0200

    Minor changes

--- src/PrecisionMatrix.jl
+++ src/PrecisionMatrix.jl
@@ -4,7 +4,6 @@ include("rotation_matrix.jl")
 include("fonctions_simu.jl")
 include("prec_xia.jl")
 include("stat_test.jl")
-include("scad_mod.jl")
 include("permute_conditional.jl")
 include("precision_iwt.jl")
 
--- src/permute_conditional.jl
+++ src/permute_conditional.jl
@@ -1,3 +1,39 @@
+import NCVREG: ncvreg
+
+"""
+    function SCADmod(yvector, x, lambda)
+
+function returning the fitted values pf regression with 
+the SCAD penalty (used for conditional permutations)
+
+```R
+SCADmod = function(yvector,x,lambda){
+  regSCAD = ncvreg::ncvreg(x,yvector,
+                           penalty='SCAD',lambda=lambda)
+  fitted = cbind(1,x) %*% regSCAD\$beta
+  return(fitted)
+}
+```
+
+"""
+function scad_mod(yvector, x, λ)
+    
+  γ = 3.7
+
+  @show x
+  beta = ncvreg(x, yvector, λ, :SCAD, γ)
+    
+  @show size(x)
+  @show size(beta)
+  n = size(x)[2]
+  
+  fitted = hcat(ones(n),x) * beta
+    
+  @show size(fitted)
+  return fitted
+
+end
+
 """ 
     permute(x, n) 
 
@@ -13,8 +49,27 @@ permute(x :: Array{Float64, 2}, n) = x[randperm(n), :]
 
 uses SCAD/OLS
 
+```R
+permute.conditional = function(y,n,data.complement,estimation){ 
+    # uses SCAD/OLS
+  permutation = sample(n)
+
+  # SCAD/OLS estimaytion
+  fitted = switch(estimation,
+                  LM=lm.fit(cbind(1,data.complement),y)\$fitted,
+                  SCAD=apply(y,2,SCADmod,x=data.complement,
+                             lambda=2*sqrt(var(y[,1])*log(ncol(data.complement))/nrow(data.complement))))
+
+  residuals = y - fitted
+
+  result = fitted + residuals[permutation,]
+  return(result)
+}
+```
+
 """
 function permute_conditional(y, n, data_complement, estimation)
+
     # SCAD/OLS estimation
     if estimation == :LM
         XX = hcat(ones(n), data_complement)
@@ -24,10 +79,13 @@ function permute_conditional(y, n, data_complement, estimation)
     elseif estimation == :SCAD
         nrows, ncols = size(data_complement)
         λ = 2*sqrt(var(y[:,1]) * log(ncols)/nrows)
-        SCAD = [scad_mod(v, data_complement,λ) for v in eachcol(y)]
+        fitted = [scad_mod(v, data_complement,λ) for v in eachcol(y)]
     end    
 
+    @show size(y)
+    @show size(fitted)
     residuals = y .- fitted
     
     return fitted .+ residuals[permutation,:]
+
 end
