3750a02e391c67706a7df11468a8d9cfdcd8d3bb
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-04-25 21:12:27 +0200

    add doc

--- src/stat_test.jl
+++ src/stat_test.jl
@@ -2,6 +2,16 @@
     stat_test(block1_perm, block2, data_orig, points_x, points_y)
 
 We use the Xia estimator for the precision matrix
+
+```R
+stat.test = function(block1.perm,block2,data.orig,points.x,points.y){
+   data.orig[,points.x] = block1.perm
+   PrecMat = PrecXia(data.orig)
+   submat = PrecMat\$TprecStd[points.x,points.y]
+   return(sum(submat)^2) # We use the Xia estimator for the precision matrix
+}
+```
+
 """
 function stat_test(block1_perm, block2, data_orig, points_x, points_y)
     
