77baeb21921b92ec7ceaec1950e6c9024728aa41
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-04-26 21:30:42 +0200

    Update iwt_block_precision.jl

--- notebooks/iwt_block_precision.jl
+++ notebooks/iwt_block_precision.jl
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-using Pkg
-pkg"add https://github.com/pnavaro/NCVREG.jl"
-
 using CategoricalArrays
 using Random
 using LinearAlgebra
@@ -12,14 +9,15 @@ using NCVREG
 using ProgressMeter
 using InvertedIndices
 using Statistics
+using UnicodePlots
 
 include("../src/rotation_matrix.jl")
 include("../src/fonctions_simu.jl")
 
 # +
 p = 20 
-n = 500
-b = 3
+n = 1000
+b = 10
 rng = MersenneTwister(42)
 
 function generate_data(rng, p, n, b)
@@ -30,11 +28,12 @@ function generate_data(rng, p, n, b)
     #hm = plot(layout=(1,2))
     #heatmap!(hm[1,1], covmat, c=ColorGradient([:red,:blue]), aspect_ratio=:equal)
     #heatmap!(hm[1,2], premat, aspect_ratio=:equal)
+    
     d = MvNormal(covmat)
     p_part = map( length,  indblocs)
     blocks  = vcat([repeat([i], j) for (i,j) in zip(1:b, p_part)]...)
     data = rand!(rng, d, zeros(Float64,(p, n)))
-    data, blocks
+    transpose(data), blocks
 end
 
 # -
@@ -42,9 +41,9 @@ end
 data, blocks = generate_data(rng, p, n, b)
 
 # +
-function permute!(rng, x :: Array{Float64, 2}) 
-   n = size(x)[1]
-   x .= x[randperm(rng, n), :] 
+function permute(rng, x, n) 
+   result = copy(x)
+   result[randperm(rng, n), :]
 end
 
 function permute_conditional(rng :: AbstractRNG, Y, Z)
@@ -70,42 +69,52 @@ end
 
 
 # +
-function stat_test(block1_perm, block2, data_orig, points_x, points_y)
+function stat_test(block1_perm, block2, X, points_x, points_y)
     
-    data_orig[:, points_x] .= block1_perm
-    nrows, ncols = size(data_orig)
+    X[:, points_x] .= block1_perm
 
-    betahat = zeros(Float64,(ncols-1,ncols))
-    reshat  = copy(data_orig)
+    n, p = size(X)
+    Tprec = zeros(p, p)
+    TprecStd = zeros(p, p)
+
+    betahat = zeros(Float64,(p-1,p))
+    reshat  = copy(X)
+    y = zeros(Float64, n)
     
-    for k in 1:ncols
-        x = view(data_orig, :, Not(k))
-        y = view(data_orig, :, k )
-        λ = [2*sqrt(var(y)*log(ncols)/nrows)]
+    @inbounds for k in 1:p
+        x = view(X, :, Not(k))
+        y .= X[:, k]
+        λ = [2*sqrt(var(y)*log(p)/n)]
         fitreg = glmnet(x, y, lambda = λ, standardize=false)
+        y .= vec(GLMNet.predict(fitreg,x))
         betahat[:,k] .= vec(fitreg.betas)
-        reshat[:,k]  .= y .- vec(GLMNet.predict(fitreg,x))
+        for i in eachindex(y)
+           reshat[i,k] = X[i, k] - y[i]
+        end
     end
     
-    rtilde = cov(reshat) .* (nrows-1) ./ nrows
+    rtilde = cov(reshat) .* (n-1) ./ n
     rhat   = rtilde
     
-    for i in 1:ncols-1
-        for j in (i+1):ncols
+    @inbounds for i in 1:p-1
+        for j in (i+1):p
             rhat[i,j] = -(rtilde[i,j]+rtilde[i,i]*betahat[i,j]+rtilde[j,j]*betahat[j-1,i])
             rhat[j,i] = rhat[i,j]
         end
     end
     
-    Tprec    = copy(1 ./ rhat)
-    TprecStd = copy(Tprec)
-    
-    for i in 1:(ncols-1), j in (i+1):ncols
-        Tprec[i,j] = rhat[i,j]/(rhat[i,i]*rhat[j,j])
-        Tprec[j,i] = Tprec[i,j]
-        thetahatij = (1+(betahat[i,j]^2  * rhat[i,i]/rhat[j,j]))/(nrows*rhat[i,i]*rhat[j,j])   
-        TprecStd[i,j] = Tprec[i,j]/sqrt(thetahatij)
-        TprecStd[j,i] = TprecStd[i,j] 
+    @inbounds for i in eachindex(Tprec, rhat)
+        Tprec[i] = 1 / rhat[i]
+    end
+
+    TprecStd .= Tprec
+
+    for i in 1:(p-1)
+        for j in (i+1):p
+            Tprec[i,j] = Tprec[j,i] = rhat[i,j]/(rhat[i,i]*rhat[j,j])
+            thetahatij = (1+(betahat[i,j]^2  * rhat[i,i]/rhat[j,j]))/(n*rhat[i,i]*rhat[j,j])   
+            TprecStd[i,j] = TprecStd[j,i] = Tprec[i,j]/sqrt(thetahatij)
+        end
     end
 
     submat = TprecStd[points_x, points_y]
@@ -116,9 +125,8 @@ end
 
 function iwt_block_precision(data, blocks; B=1000)
     
-    
     estimation = :SCAD
-    p, n  = size(data)
+    n, p  = size(data)
     nblocks = length(levels(CategoricalArray(blocks)))
     # tests on rectangles
     pval_array = zeros(Float64,(2,p,p))
@@ -132,8 +140,8 @@ function iwt_block_precision(data, blocks; B=1000)
     Tperm_tmp = zeros(Float64, B)
 
     # x coordinate starting point and length on x axis of the rectangle
-    # for ix in 2:nblocks, lx in 0:(nblocks-ix)
-    for ix in 2:2, lx in 0:0
+    @showprogress 1 for ix in 2:nblocks, lx in 0:(nblocks-ix)
+
         # FIRST BLOCK
 
         index_x = ix:(ix+lx) # index first block
@@ -142,17 +150,13 @@ function iwt_block_precision(data, blocks; B=1000)
         data_B1_array = Iterators.repeated(data_B1, B)
 
         # y coordinate starting point. stops before the diagonal and length on y axis of the rectangle
-        # for iy in 1:(ix-1), ly in 0:(ix-iy-1)
-        for iy in 1:1, ly in 0:0
+        for iy in 1:(ix-1), ly in 0:(ix-iy-1)
+
             # SECOND BLOCK
 
             index_y = iy:(iy+ly) # index second block
             points_y = findall( x -> x in index_y, blocks)
-            data_B2 = view(data,:,points_y)
-            
-            @show nblocks
-            @show index_x
-            @show index_y
+            data_B2 = data[:,points_y]
             index_complement = collect(filter(index -> !(index in vcat(index_x,index_y)), 1:nblocks))
             points_complement = findall( x -> x in index_complement, blocks)
             data_complement = data[:,points_complement]
@@ -169,13 +173,16 @@ function iwt_block_precision(data, blocks; B=1000)
             fill!(testmatrix, 0.0) 
             testmatrix[points_x,points_y] .= 1
             ntests_blocks .+= testmatrix
+
+            heatmap(ntests_blocks, title="Blocks: $index_x - $index_y")
+            heatmap(testmatrix,    title="Blocks: $index_x - $index_y")
             
             T0_tmp = stat_test(data_B1,data_B2,data,points_x,points_y)
 
             fill!(Tperm_tmp, 0.0)
 
-            @showprogress 1 for i in eachindex(Tperm_tmp)
-                Tperm_tmp[i] = stat_test(data_B1_perm[i], data_B2, data, points_x, points_y)
+            Threads.@threads for i in eachindex(Tperm_tmp)
+                @inbounds Tperm_tmp[i] = stat_test(data_B1_perm[i], data_B2, data, points_x, points_y)
             end
 
             pval_tmp = mean(Tperm_tmp .>= T0_tmp)
@@ -189,7 +196,7 @@ function iwt_block_precision(data, blocks; B=1000)
             # pval_array[2,:,:] .= corrected_pval_temp 
             # maximization for updating the adjusted p-value
 
-            for i in 1:20, j in 1:20
+            for i in 1:p, j in 1:p
                 corrected_pval[i,j] = max(corrected_pval[i,j], corrected_pval_temp[i,j])
             end
 
@@ -205,6 +212,8 @@ function iwt_block_precision(data, blocks; B=1000)
     corrected_pval
 end
 
-iwt_block_precision(data, blocks; B=100)
+@time res = iwt_block_precision(data, blocks; B=1000)
+
+heatmap(res)
 
 
