6fdca1c429fbe5735c1b93ea03e41a8dc85b3ab3
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-04-27 17:30:55 +0200

    Update iwt_block_precision.jl

--- notebooks/iwt_block_precision.jl
+++ notebooks/iwt_block_precision.jl
@@ -10,244 +10,15 @@ using InvertedIndices
 using Statistics
 using UnicodePlots
 
+include("../src/rotation_matrix.jl")
+include("../src/structure_cov.jl")
+include("../src/cov_simu.jl")
+include("../src/generate_data.jl")
+include("../src/stat_test.jl")
+include("../src/permute_conditional.jl")
 
-# +
-function RotationMatrix(rng, p :: Int64)
-
-    if p == 1 return (1.0 .* Matrix(I,1,1)) end
-
-    P = zeros(Float64, (p,p)) 
-
-    if p == 2
-        u = rand(rng)
-        sqrt_1_u2 = sqrt(1 - u*u)
-        P[1,1] = sqrt_1_u2;    # P[0, 0] = sqrt(1 - u^2) = cos(theta)
-        P[2,1] = u;            # P[1, 0] = u = sin(theta)
-        P[1,2] = -u;           # P[0, 1] = -u = -sin(theta)
-        P[2,2] = sqrt_1_u2;    # P[1, 1] = sqrt(1 - u^2) = cos(theta)
-        return P
-    end
-
-    Q, R = qr(rand(rng, p, p))
-    P = collect(Q)
-
-    if p % 2 == 0
-
-        if first(P) < 0 
-           P .*= -1
-        end
-
-        E = 1.0 .* Matrix(I, p, p)
-        for k in 2:p
-            for j in 2:k, i in 1:p
-                E[i,j] = P[i,j]
-            end
-            if det(E) < 0
-                P[:,k] .*= -1
-            end
-        end 
-    end
-  
-    return P
-
-end
-
-# +
-"""
-    structure_cov(p, blocs_on; b = 10, seed = p)
-
-# Fonction pour definir les indices des blocs
-
-  - p : dimension des donnees
-  - b : nombre de blocs
-  - blocs_on : liste des indices des couples de blocs "allumes"
-"""
-function structure_cov(rng :: AbstractRNG,
-                       p :: Int, 
-                       b :: Int, 
-                       blocs_on :: Vector{Vector{Int}} )
-  
-  blocs = sample(rng, 3:(p-2), (b-1), replace=false)
-  sort!(blocs)
-  blocs = [1, blocs..., p]
-  indblocs = UnitRange{Int64}[]
-  push!(indblocs, 1:blocs[2])
-
-  for i in 2:(length(blocs)-1) 
-    push!(indblocs, (blocs[i]+1):blocs[i+1])
-  end
-  
-  matBlocs = zeros(Int64, (p, p))
-
-  for l in indblocs
-      matBlocs[l, l] .= 1
-  end
-  
-  for bloc in blocs_on
-      for i in indblocs[bloc[1]]
-          for j in indblocs[bloc[2]]
-              matBlocs[i, j] = 1
-              matBlocs[j, i] = 1
-          end
-      end
-  end
-  
-  return blocs, indblocs, matBlocs
-
-end
-
-
-# +
-"""
-    cov_simu(blocs, indblocs, blocs_on, D)
-
-# fonction pour simuler des matrices de covariances avec blocs
-  - blocs    : indice des intervalles separant les blocs (sortie de StructureCov)
-  - indblocs : liste des indices des blocs (sortie de StructureCov)
-  - blocs_on  : liste des indices des couples de blocs "allumes"
-  - D        : vecteur des valeurs propres de la matrice a simuler
-"""
-function cov_simu(rng, blocs, indblocs, blocs_on, D)
-  
-  b = length(blocs) - 1
-  p = sum(map(length,indblocs))
-  P = zeros(Float64, (p, p))
-  
-  # matrices de rotation pour les blocs "allumes"
-  for i in eachindex(blocs_on)
-      B1 = indblocs[blocs_on[i][1]]
-      B2 = indblocs[blocs_on[i][2]]
-      nB = length(B1) + length(B2)
-      P[[B1..., B2...], [B1..., B2...]] .= RotationMatrix(rng, nB)
-  end
-  
-  # matrices de rotation pour les autre blocs centraux (non allumes)
-  for i in setdiff(1:b, Iterators.flatten(blocs_on))
-      P[indblocs[i], indblocs[i]] .= RotationMatrix(rng, length(indblocs[i]))
-  end
-  
-  for i in 1:length(blocs_on)
-    P[indblocs[blocs_on[i][1]] , indblocs[blocs_on[i][2]]] .= 0
-  end
-
-  P .= transpose(P)
-  
-  # Matrice de covariance
-  CovMat = Hermitian(P' * Diagonal(D) * P)
-  
-  # Matrice de precision
-  PreMat = P' * Diagonal(1 ./ D) * P
-  
-  return collect(CovMat), PreMat
-
-end
-
-# +
-
-function generate_data(rng, p, n, b, blocs_on )
-
-    blocs, indblocs = structure_cov(rng, p, b, blocs_on)
-
-    D = rand(rng, Uniform(1e-4, 1e-2), p)
-
-    covmat, premat = cov_simu(rng, blocs, indblocs, blocs_on, D)
+function iwt_block_precision(rng, data, blocks; B=1000)
     
-    d = MvNormal(covmat)
-    p_part = map( length,  indblocs)
-    blocks  = vcat([repeat([i], j) for (i,j) in zip(1:b, p_part)]...)
-    data = rand!(rng, d, zeros(Float64,(p, n)))
-    covmat, premat, transpose(data), blocks
-
-end
-
-
-
-# +
-function permute(rng, x, n) 
-   result = copy(x)
-   result[randperm(rng, n), :]
-end
-
-function permute_conditional(rng :: AbstractRNG, Y, Z)
-    row_y, col_y = size(Y)
-    row_z, col_z = size(Z)
-    
-    @assert row_y == row_z
-    
-    fitted = similar(Y)
-    for j in 1:col_y
-        y = Y[:,j]
-        λ = 2*sqrt(var(Y[:,1])*log(col_z)/row_z)
-        beta = NCVREG.coef(NCVREG.SCAD(Z, y, [λ]))  
-        fitted[:,j] .= vec(hcat(ones(row_z),Z) * beta)
-    end
-    
-    residuals = Y .- fitted
-    
-    permutation = randperm(rng, row_y)
-        
-    return fitted .+ residuals[permutation,:]
-end
-
-
-# +
-function stat_test(block1_perm, block2, X, points_x, points_y)
-    
-    X[:, points_x] .= block1_perm
-
-    n, p = size(X)
-    Tprec = zeros(p, p)
-    TprecStd = zeros(p, p)
-
-    betahat = zeros(Float64,(p-1,p))
-    reshat  = copy(X)
-    y = zeros(Float64, n)
-    
-    @inbounds for k in 1:p
-        x = view(X, :, Not(k))
-        y .= X[:, k]
-        λ = [2*sqrt(var(y)*log(p)/n)]
-        fitreg = glmnet(x, y, lambda = λ, standardize=false)
-        y .= vec(GLMNet.predict(fitreg,x))
-        betahat[:,k] .= vec(fitreg.betas)
-        for i in eachindex(y)
-           reshat[i,k] = X[i, k] - y[i]
-        end
-    end
-    
-    rtilde = cov(reshat) .* (n-1) ./ n
-    rhat   = rtilde
-    
-    @inbounds for i in 1:p-1
-        for j in (i+1):p
-            rhat[i,j] = -(rtilde[i,j]+rtilde[i,i]*betahat[i,j]+rtilde[j,j]*betahat[j-1,i])
-            rhat[j,i] = rhat[i,j]
-        end
-    end
-    
-    @inbounds for i in eachindex(Tprec, rhat)
-        Tprec[i] = 1 / rhat[i]
-    end
-
-    TprecStd .= Tprec
-
-    for i in 1:(p-1)
-        for j in (i+1):p
-            Tprec[i,j] = Tprec[j,i] = rhat[i,j]/(rhat[i,i]*rhat[j,j])
-            thetahatij = (1+(betahat[i,j]^2  * rhat[i,i]/rhat[j,j]))/(n*rhat[i,i]*rhat[j,j])   
-            TprecStd[i,j] = TprecStd[j,i] = Tprec[i,j]/sqrt(thetahatij)
-        end
-    end
-
-    submat = TprecStd[points_x, points_y]
-    return sum(submat).^2
-    
-end
-# -
-
-function iwt_block_precision(data, blocks; B=1000)
-    
-    estimation = :SCAD
     n, p  = size(data)
     nblocks = length(levels(CategoricalArray(blocks)))
     # tests on rectangles
@@ -287,9 +58,9 @@ function iwt_block_precision(data, blocks; B=1000)
             ncol = size(data_complement)[2]
 
             if ncol > 0
-                data_B1_perm = [permute_conditional(rng, B1, data_complement) for B1 in data_B1_array];
+                data_B1_perm = map( B1 -> permute_scad(rng, B1, data_complement), data_B1_array)
             else
-                data_B1_perm = [permute(rng, B1, n) for B1 in data_B1_array];
+                data_B1_perm = map( B1 -> permute(rng, B1, n),  data_B1_array)
             end
 
             fill!(testmatrix, 0.0) 
@@ -335,26 +106,17 @@ function iwt_block_precision(data, blocks; B=1000)
 
 end
 
-p = 10 
-n = 500
-b = 3
+p = 20 
+n = 1000
+b = 5
 rng = MersenneTwister(12)
 blocs_on  = [[1,3]]
 
 covmat, premat, data, blocks = generate_data(rng, p, n, b, blocs_on)
 
-
 display(heatmap(covmat, title="covmat"))
 display(heatmap(premat, title="premat"))
 
-@show blocks
+@time res = iwt_block_precision(rng, data, blocks; B=1000)
 
-@time res = iwt_block_precision(data, blocks; B=1000)
-
-display(heatmap(res, height=30, width=30))
-
-println(res)
-
-@show covmat
-
-@show premat
+display(heatmap(res))
