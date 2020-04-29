bb78f2211551a5cb02d5d0c372286470c0e8d86f
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-04-29 18:00:59 +0200

    Update precision_iwt.jl

--- src/precision_iwt.jl
+++ src/precision_iwt.jl
@@ -13,32 +13,21 @@ function performing the test on blocks and adjusting the results
 """
 function iwt_block_precision(rng :: AbstractRNG, data, blocks; B=1000)
     
-    estimation = :SCAD
     n, p  = size(data)
     nblocks = length(levels(CategoricalArray(blocks)))
-    # tests on rectangles
-    # pval_array = zeros(Float64,(2,p,p))
     corrected_pval = zeros(Float64, (p,p))
-    # responsible_test = Array{String,2}(undef,p,p)
-    ntests_blocks = zeros(Float64,(p,p))
-    testmatrix = zeros(Float64,(p,p))
-    index = zeros(Int,(p,p))
-    corrected_pval_temp = zeros(Float64,(p,p))
-    Tperm_tmp = zeros(Float64, B)
+    Tperm = zeros(Float64, B)
 
     stat_test = StatTest(n, p)
     
     # x coordinate starting point and length on x axis of the rectangle
     for ix in 2:nblocks, lx in 0:(nblocks-ix)
 
-
         # FIRST BLOCK
-
         index_x = ix:(ix+lx) # index first block
         points_x = findall(x -> x in index_x, blocks) # coefficients in block index.x
 
         data_B1 = view(data,:,points_x) # data of the first block
-
 
         # y coordinate starting point. stops before the diagonal and length on y axis of the rectangle
         for iy in 1:(ix-1), ly in 0:(ix-iy-1)
@@ -60,39 +49,24 @@ function iwt_block_precision(rng :: AbstractRNG, data, blocks; B=1000)
             # permuted data of the first block
             ncol = size(data_complement)[2]
 
-            fill!(testmatrix, 0.0) 
-            testmatrix[points_x,points_y] .= 1
-            ntests_blocks .+= testmatrix
+            T0 = stat_test(data_B1, data, points_x, points_y)
 
-            T0_tmp = stat_test(data_B1, data, points_x, points_y)
-
-            for i in eachindex(Tperm_tmp)
+            for i in eachindex(Tperm)
                 if ncol > 0
                     data_B1_perm = permute_scad(rng, data_B1, data_complement)
                 else
                     data_B1_perm = permute(rng, data_B1, n)
                 end
-                Tperm_tmp[i] = stat_test(data_B1_perm, data, points_x, points_y)
+                Tperm[i] = stat_test(data_B1_perm, data, points_x, points_y)
             end
 
-            pval_tmp = mean(Tperm_tmp .>= T0_tmp)
+            pval = mean(Tperm .>= T0)
 
-            corrected_pval_temp[points_x,points_y] .= pval_tmp
-            corrected_pval_temp[points_y,points_x] .= pval_tmp # symetrization
+            for i in points_x, j in points_y
+                corrected_pval[i,j] = max(corrected_pval[i,j], pval)
+                corrected_pval[j,i] = corrected_pval[i,j]
+            end
 
-            # old adjusted p-value
-            # pval_array[1,:,:] .= corrected_pval 
-            # p-value resulting from the test of this block
-            # pval_array[2,:,:] .= corrected_pval_temp 
-            # maximization for updating the adjusted p-value
-            #for i in 1:p, j in 1:p 
-            #    index[i,j] = findmax(view(pval_array,:,i,j))[2]
-            #end
-
-            #responsible_test[ index .== 2] .= "$(paste(index_x))-$(paste(index_y))"
-
-            corrected_pval = max.(corrected_pval, corrected_pval_temp)
-            
         end
     end
     
