b915d850cc18fa56c464c61b2b875abd51a3c04c
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-05-09 21:06:51 +0200

    Update precision_iwt.jl

--- src/precision_iwt.jl
+++ src/precision_iwt.jl
@@ -1,6 +1,86 @@
 using CategoricalArrays
 using Random
 
+
+function index_blocks(blocks)
+
+    nblocks = length(levels(CategoricalArray(blocks)))
+
+    index_xy = []
+
+    for ix in 2:nblocks, lx in 0:(nblocks-ix)
+        index_x = ix:(ix+lx)
+        for iy in 1:(ix-1), ly in 0:(ix-iy-1)
+            index_y = iy:(iy+ly)
+            push!(index_xy, (index_x, index_y))
+        end
+    end
+
+    index_xy
+
+end
+
+function compute_pval!(pval, rng :: AbstractRNG, data, 
+                        stat_test, blocks, index_x, index_y; b = 1000)
+    
+    n, p = size(data)
+    
+    points_x = findall(x -> x in index_x, blocks) # coefficients in block index.x
+    data_x = view(data,:,points_x) # data of the first block
+    data_x_perm = copy(data_x)
+    residuals = similar(data_x_perm)
+    points_y = findall( x -> x in index_y, blocks)
+    nblocks = length(levels(CategoricalArray(blocks)))
+    index_z = filter(index -> !(index in vcat(index_x,index_y)), 1:nblocks)
+    points_z = findall( x -> x in index_z, blocks)
+    data_z = view(data, :, points_z)
+    permutation = zeros(Int, n)
+    tperm = zeros(b)
+
+    ncol = length(points_z)
+
+    t0 = stat_test(data_x, data, points_x, points_y)
+
+    zz = hcat(ones(n), data_z)
+
+    for i in eachindex(tperm)
+
+        permutation .= randperm(rng, n)
+
+        if ncol > 0
+
+            for j in 1:length(points_x)
+                y = view(data_x,:,j)
+                λ = 2*sqrt(var(view(data_x,:,1))*log(length(points_z))/n)
+                β = NCVREG.coef(NCVREG.SCAD(data_z, y, [λ]))
+                data_x_perm[:,j] .= vec(zz * β)
+            end
+                
+            residuals .= data_x .- data_x_perm
+                
+            data_x_perm .+= view(residuals, permutation, :)
+                    
+        else
+
+            data_x_perm .= data[permutation, points_x]
+
+        end
+
+        tperm[i] = stat_test(data_x_perm, data, points_x, points_y)
+
+    end
+
+    pvalue = mean(tperm .>= t0)
+
+    for i in points_x, j in points_y
+        pval[i,j] = max(pval[i,j], pvalue)
+        pval[j,i] = pval[i,j]
+    end
+
+    pval
+
+end
+
 """
     iwt_block_precision(data, blocks; B=1000, estimation=:SCAD)  
 
@@ -15,84 +95,18 @@ function iwt_block_precision(rng :: AbstractRNG, data, blocks; B=1000)
     
     n, p  = size(data)
     nblocks = length(levels(CategoricalArray(blocks)))
-    corrected_pval = zeros(Float64, (p,p))
-    Tperm = zeros(Float64, B)
+    pval = zeros(Float64, (p,p))
 
     stat_test = StatTest(n, p)
-    permutation = randperm(rng, n)
+
+    index_xy = index_blocks(blocks)
     
-    # x coordinate starting point and length on x axis of the rectangle
-    for ix in 2:nblocks, lx in 0:(nblocks-ix)
+    for (index_x, index_y) in index_xy
 
-        # FIRST BLOCK
-        index_x = ix:(ix+lx) # index first block
-        points_x = findall(x -> x in index_x, blocks) # coefficients in block index.x
+        compute_pval!(pval, rng, data, stat_test, blocks, index_x, index_y)
 
-        data_B1 = view(data,:,points_x) # data of the first block
-        data_B1_perm = copy(data[:, points_x])
-        residuals = similar(data_B1_perm)
-
-        # y coordinate starting point. stops before the diagonal and length on y axis of the rectangle
-        for iy in 1:(ix-1), ly in 0:(ix-iy-1)
-
-            # SECOND BLOCK
-
-            index_y = iy:(iy+ly) # index second block
-            points_y = findall( x -> x in index_y, blocks)
-
-            # data_B2 = view(data,:,points_y) # data of the second block
-
-            # COMPLEMENT
-
-            index_complement = filter(index -> !(index in vcat(index_x,index_y)), 1:nblocks)
-            points_complement = findall( x -> x in index_complement, blocks)
-
-            data_complement = view(data, :, points_complement)
-
-            # permuted data of the first block
-            ncol = size(data_complement)[2]
-
-            T0 = stat_test(data_B1, data, points_x, points_y)
-
-            ZZ = hcat(ones(n), data_complement)
-
-            for i in eachindex(Tperm)
-
-                permutation .= randperm(rng, n)
-
-                if ncol > 0
-
-                    for j in 1:length(points_x)
-                        y = view(data_B1,:,j)
-                        λ = 2*sqrt(var(view(data_B1,:,1))*log(length(points_complement))/n)
-                        β = NCVREG.coef(NCVREG.SCAD(data_complement, y, [λ]))
-                        data_B1_perm[:,j] .= vec(ZZ * β)
-                    end
-                    
-                    residuals .= data_B1 .- data_B1_perm
-                    
-                    data_B1_perm .+= view(residuals, permutation, :)
-                        
-                else
-
-                    data_B1_perm .= data[permutation, points_x]
-
-                end
-
-                Tperm[i] = stat_test(data_B1_perm, data, points_x, points_y)
-
-            end
-
-            pval = mean(Tperm .>= T0)
-
-            for i in points_x, j in points_y
-                corrected_pval[i,j] = max(corrected_pval[i,j], pval)
-                corrected_pval[j,i] = corrected_pval[i,j]
-            end
-
-        end
     end
     
-    corrected_pval
+    pval
 
 end
